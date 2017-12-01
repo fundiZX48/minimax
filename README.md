5b7acbcafad1a5c2cbb58d42007317e73cec8041
Mark Mitterdorfer <mark_mitter@yahoo.co.uk>
2017-12-01 11:49:03 +0000

    Added visualization

--- README.md
+++ README.md
@@ -7,6 +7,9 @@ Using conda to install NetworkX:
 
     conda install -c anaconda networkx
 
+or if you prefer pip:
+
+    pip install networkx
 
 run the example from the src folder:
 
@@ -17,19 +20,46 @@ run the example from the src folder:
 
 NetworkX is a great Python library for managing and traversing graph based structures.
 
-Use G.add_node to add a node to the graph. Set *terminal* to *None* if the node is not a terminal value, otherwise set to a valid integer score:
+Use *G.add_node()* to add a node to the graph. Set *terminal* to *None* if the node is not a terminal value, otherwise set to a valid integer or float score:
 
 ```python
-G.add_node("nodeName", terminal="valueOfTerminal")
+G.add_node("nodeName", terminal=int_or_float_value)
 ```
 
-Use G.add_edge to add an edge from a source node to a destination node:
+Use *G.add_edge()* to add an edge from a source node to a destination node:
 
 ```python
 G.add_edge("sourceNode", "destinationNode")
 ```
 
-## Todo
+## Visualizing the graph
 
-Add some visualisation for the graph. At the moment is it all pretty much pen and paper approach!
+Check the correctness of your graph by visualizing it. You will require pydot and Graphviz. If running on Ubuntu/Linux you require Graphviz:
+
+    sudo apt install graphviz
+
+And pydot Python installation using pip:
+
+    pip install pydot
+
+(conda does not seem to support pydot and Python 3)
+
+And the Python code to accomplish this:
+
+```python
+    p = nx.drawing.nx_pydot.to_pydot(G)
+    p.write_png('output_filename.png')
+```
+
+And below is a graph example from the *complex_tree()* function: 
+
+![complex_tree](/src/complex_tree.png)
+
+For sake of clarity and to highlight terminal nodes in the graph visualization, I have annotated the *add_node()* methods like this:
+
+```python
+G.add_node("nodeName=terminal", terminal=int_or_float_value)
+```
+
+That is add the terminal value to the name of the node. Note, *add_edge()* should also correspond to the annotated node name.
 
--- src/minimax.py
+++ src/minimax.py
@@ -42,13 +42,18 @@ def simple_tree():
     G = nx.DiGraph()
 
     G.add_node("root", terminal=None)
-    G.add_node("a", terminal=-1)
+    G.add_node("a=-1", terminal=-1)
     G.add_node("b", terminal=None)
-    G.add_node("c", terminal=3)
+    G.add_node("c=3", terminal=3)
 
-    G.add_edge("root", "a")
+    G.add_edge("root", "a=-1")
     G.add_edge("root", "b")
-    G.add_edge("b", "c")
+    G.add_edge("b", "c=3")
+
+    # Remove the below two lines if you prefer not to visualize
+    # or you do not have pydot or Graphviz installed
+    p = nx.drawing.nx_pydot.to_pydot(G)
+    p.write_png('simple_tree.png')
 
     print("Best path to take in simple_tree():", minimax(G, n="root"))
 
@@ -62,27 +67,32 @@ def complex_tree():
     G.add_node("a", terminal=None)
     G.add_node("b", terminal=None)
     G.add_node("c", terminal=None)
-    G.add_node("d", terminal=7)
-    G.add_node("e", terminal=3)
-    G.add_node("f", terminal=15)
-    G.add_node("g", terminal=1)
-    G.add_node("h", terminal=12)
-    G.add_node("i", terminal=20)
-    G.add_node("j", terminal=22)
+    G.add_node("d=7", terminal=7)
+    G.add_node("e=3", terminal=3)
+    G.add_node("f=15", terminal=15)
+    G.add_node("g=1", terminal=1)
+    G.add_node("h=12", terminal=12)
+    G.add_node("i=20", terminal=20)
+    G.add_node("j=22", terminal=22)
 
     G.add_edge("root", "a")
     G.add_edge("root", "b")
     G.add_edge("root", "c")
 
-    G.add_edge("a", "d")
-    G.add_edge("a", "e")
+    G.add_edge("a", "d=7")
+    G.add_edge("a", "e=3")
 
-    G.add_edge("b", "f")
+    G.add_edge("b", "f=15")
 
-    G.add_edge("c", "g")
-    G.add_edge("c", "h")
-    G.add_edge("c", "i")
-    G.add_edge("c", "j")
+    G.add_edge("c", "g=1")
+    G.add_edge("c", "h=12")
+    G.add_edge("c", "i=20")
+    G.add_edge("c", "j=22")
+
+    # Remove the below two lines if you prefer not to visualize
+    # or you do not have pydot or Graphviz installed
+    p = nx.drawing.nx_pydot.to_pydot(G)
+    p.write_png('complex_tree.png')
 
     print("Best path to take in complex_tree():", minimax(G, n="root"))
 
